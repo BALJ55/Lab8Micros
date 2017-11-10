f6de3bc8472ec9b348e9dbe6ef79c2bbc454d810
BALJ55 <byron@gtxsolutions.com>
2017-11-09 19:03:29 -0600

    archivos mx

--- kernel.cu
+++ kernel.cu
@@ -1,141 +1,134 @@
 #include "kernel.h"
-
 #include <stdio.h>
 
-
 #define TX 32
-
 #define TY 32
-
 #define RAD 1
 
-
-/* 
+/*  
 int divUp(int a, int b){
-
 	return (a + b - 1)/b;
-
 }
-
 */
 // clip values to [0 , 255]
-
 __device__ unsigned char clip(int n){
-
   return n > 255 ? 255 : (n < 0 ? 0 : n);
-
 }
-
 
 // bound index values to max size
-
 __device__ int idxClip(int idx, int idxMax){
-
  return idx >(idxMax - 1) ? (idxMax - 1) : (idx < 0 ? 0 : idx);
-
 }
-
 
 // transform img(c,r) to flat index i
-
 __device__ int flatten(int col, int row, int width, int height) {
-
   return idxClip(col, width) + idxClip(row, height)*width;
-
 }
 
 
+__global__ void filter_kernel(unsigned char *d_input, unsigned char *d_output, int rows, int cols, float* d_window, int window_size) {
 
-__global__ void
-filter_kernel(unsigned char *d_input, unsigned char *d_output, int rows, int cols,float* d_window, int window_size) {
 
+  //const int s_idx = threadIdx.x + RAD;
+  extern __shared__ float smem[];
+  const int s_idx = threadIdx.x + RAD;
 
   //define image row, col position
-
   const int c = threadIdx.x + blockDim.x * blockIdx.x;
-
   const int r = threadIdx.y + blockDim.y * blockIdx.y;
-  // exit if out of image bounds
 
+  // exit if out of image bounds
   if((c >= cols) || (r >= rows)) return;
 
-
   // compute flat index
-
   const int i = flatten(c, r, cols, rows);
 
-
-  const int gloc= threadIdx.x + RAD;
-  extern_shared_float data[];
-
-  data[gloc]= d_input[i];
-
-  if (threadIdx.x <RAD){
-      data[gloc -RAD]-d_input[i-RAD];
-      data[gloc +blockDim.x] - d_input[i +blockDim.x];
-  }
-  _syncthreads();
-
-
+  smem[s_idx] = d_input[i];
+ 
   float pixel_result = 0;
-  //se aplica la convolucion a la imagen para esto se llegara a utilizar un for 
-  for(int rd = -RAD; rd<= RAD; ++rd){
-   for(int cd = -RAD; cd<= RAD; ++cd){
-  //compute image and windows indexes
-  int imgIdx= flatten(c +cd, r +rd, cols, rows);
-  int fltIdx= flatten(RAD + cd, RAD +rd, window_size);  
-  // lee la funcion del pixel y los indexes de la ventana
-  uchar pixel_val =i_input[imgIdx];
-  float weight = d_window[fltIdx];
-  //se acumula el valor
-  pixel_result += pixel_val * weight;
- }
-}
-//bound pixel value to [0,255] and store in output
-d_output[i] = clip((uchar) pixel_result);
+
+  // apply the convolution window to the image
+  // -----------------------------------------
+  for(int rd = -RAD; rd <= RAD; ++rd){
+    for(int cd = -RAD; cd <= RAD; ++cd){
+      
+      // compute image and window indexes
+      int imgIdx = flatten(c + cd, r + rd, cols, rows);
+      int fltIdx = flatten(RAD + cd, RAD + rd, window_size, window_size);
+
+      // read pixel value and window constant
+      uchar pixel_val = d_input[imgIdx];
+      float weight = d_window[fltIdx];
+
+      // accumulate value
+      pixel_result += pixel_val * weight;
+    }
+  }
+ 
+ // bound pixel value to [0 255] and store in output
+  d_output[i] = clip((uchar) pixel_result);
 }
 
-//definicion del wrapper function
+
+// wrapper function definition
 void filter_gpu(Mat input, Mat output){
-  //convolucion del window size
-  const int window_size = 2 * RAD +1;
-  const float edgeDetected[9] = {-1, -1, -1, -1, 8, -1, -1, -1, -1};
+
+  // define convoltion window size
+  const int window_size = 2 * RAD + 1;
+
+  // define convoltion window values
+
+ /*
+    const float sharpen[9] = {-1.0, -1.0, -1.0,
+                             -1.0,  9.0, -1.0,
+                             -1.0, -1.0, -1.0};
+  
+   const float gaussianBlur[9] = {1/16, 2/16, 1/16,
+                                  2/16, 4/16, 2/16,
+                                  1/16, 2/16, 1/16};
+*/
+  const float edgeDetect[9] = {-1, -1, -1,
+                               -1,  8, -1,
+                               -1, -1, -1};
+
   unsigned char *inputPtr = (unsigned char*) input.data;
   unsigned char *outputPtr = (unsigned char*) output.data;
-  unsigned char rows = input.rows;
-  unsigned char cols= input.cols;
-  //grid size dimensions (blocks)
-  int Bx = (TX +cols -1)/TX;
-  int By = (TY +rows -1)TY;
-// se declaran punteros para memoria 
-  unsigned char *d_in = 0;
-  unsigned char *d_out= 0;
+  unsigned int cols = input.cols;
+  unsigned int rows = input.rows;
+
+  // grid size dimensions (blocks)
+  int Bx = (TX + cols -1)/TX;
+  int By = (TY + rows -1)/TY;
+
+  // declare pointers to device memory
+  unsigned char *d_in  = 0;
+  unsigned char *d_out = 0;
   float *d_window = 0;
-//input y output en el device
+
+  // allocate input and output memory in device
   cudaMalloc(&d_in, cols*rows*sizeof(unsigned char));
-  cudaMalloc(&d_out, cols*rows* sizeof(unsigned char)); 
+  cudaMalloc(&d_out, cols*rows*sizeof(unsigned char));
   cudaMalloc(&d_window, window_size*window_size * sizeof(float));
 
-//de ejemplos en clase, del host al device
+  // copy input data and filter element from host to device
   cudaMemcpy(d_in, inputPtr, cols*rows*sizeof(unsigned char), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_window, edgeDetect, window_size*window_size*sizeof(float),  cudaMemcpyHosttoDevice);
-//dimensiones del kernel
-  size_t sharedMemSize = (TX +2 *RAD) *sizeof(float);
+  cudaMemcpy(d_window, edgeDetect, window_size*window_size*sizeof(float), cudaMemcpyHostToDevice);
+
+  // prepare kernel lauch dimensions
   const dim3 blockSize = dim3(TX, TY);
   const dim3 gridSize = dim3(Bx, By);
-//GPU y el kernel
-  filter_kernel<<gridSize, blockSize, sharedMemSize >>> (d_in, d_out, rows, cols, d_window, window_size);
-//copia del device al host
-  cudaMemcpy(outputPtr, d_out, rows*cols*sizeof(unsigned char) , cudaMemcpyDeviceToHost);
+
+  const size_t smemSize = ((TX * TY) + (2 * RAD) * sizeof(float));
+
+  // launch kernel in GPU
+  filter_kernel<<<gridSize, blockSize, smemSize>>>(d_in, d_out, rows, cols, d_window, window_size);
+
+  // copy output from device to host
+  cudaMemcpy(outputPtr, d_out, rows*cols*sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+  // free the memory allocated for device arrays
   cudaFree(d_in);
   cudaFree(d_out);
   cudaFree(d_window);
+
 }
-
-
-  
-
-
-
-  
-  
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 // Christian Medina Armas
-//modificado por Ana lucia Diaz Leppe 151378
 // CC3056
 // CUDA
-// filtro de memoria compartida 
+// Apply a filter to an image
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -27,15 +27,10 @@ int main(){
 
 	// create a zero filled Mat of the input image size
 	Mat output_img = Mat::zeros(Size(input_img.cols, input_img.rows), CV_8UC1);
-	//el main necesita calcular el tiempo
-	double t2 = (double) getTicketCount();
+
 	// compute filter
-	//necesitamos agregar el tiempo t2 y definir la ejecucion del tiempo
 	filter_gpu(input_img, output_img);
-	t2= ((double) getTicketCount()-t2) /getTicketFrequency();
-	count << "tiempo de ejecucion: " << t2 << " s" << end1;
-	
-	imwrite("filter_AnaluciaDiaz.jpeg", output_img);
-	cout <<"La imagen saliooo." <<end1;
+
+	imwrite("output.jpg", output_img);
 	return 0;
 }
